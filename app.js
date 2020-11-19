4cb1215b6647780e63d6f70d196ae2e27953e9c2
Benjamin Barnard <barnardwben@yahoo.com>
2020-11-19 11:17:32 +0100

    Updated UI

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const subBtn = document.querySelector('.subbtn');
 const kpi = "b149dd47b3mshb4a36f3ac098a96p1e1e49jsn41b455bd94d6";
 subBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  document.querySelector('.flight-info-container').classList.remove('opacity')
   getRatesOut();
   getRatesIn();
 })
@@ -21,35 +20,41 @@ const getRatesOut = () => {
   let departure = document.querySelector('#departure').value;
   let inbound = document.querySelector('#inbound').value;
 
-  return fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/${currency}/en-US/${origin}-sky/${destination}-sky/${departure}?inboundpartialdate=${inbound}`, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": `${kpi}`,
-      "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-    }
-  })
-    .then(res => res.json())
-    .then(data => {
+  if (currency === '' || origin === '' || destination === '' || departure === '' || inbound === '') {
+    return alert('Please Fill In All Form Inputs');
+  } else {
 
-      if (data.Places.length === 0) {
-        display.innerHTML = `
+    document.querySelector('.flight-info-container').classList.remove('opacity')
+
+    return fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/${currency}/en-US/${origin}-sky/${destination}-sky/${departure}?inboundpartialdate=${inbound}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": `${kpi}`,
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+
+        if (data.Places.length === 0) {
+          display.innerHTML = `
           <h2 class="alert">No Flights Found!</h2>
           <p>Try a different location within the same continent</p>
         `
-      } else {
-
-        let carrier = data.Carriers[0].Name;
-        let locationOne;
-        let locationTwo;
-        if (origin === data.Places[0].IataCode) {
-          locationOne = data.Places[0].Name;
-          locationTwo = data.Places[1].Name;
         } else {
-          locationOne = data.Places[1].Name;
-          locationTwo = data.Places[0].Name;
-        }
 
-        display.innerHTML = `
+          let carrier = data.Carriers[0].Name;
+          let locationOne;
+          let locationTwo;
+          if (origin === data.Places[0].IataCode) {
+            locationOne = data.Places[0].Name;
+            locationTwo = data.Places[1].Name;
+          } else {
+            locationOne = data.Places[1].Name;
+            locationTwo = data.Places[0].Name;
+          }
+
+          display.innerHTML = `
       <div class="flexer">
         <h2>${locationOne}(${origin})</h2>
         <i class="fas fa-exchange-alt"></i>
@@ -65,14 +70,14 @@ const getRatesOut = () => {
       </div>
       `
 
-        dpPrice = data.Quotes[0].MinPrice;
-      }
-    })
-    .catch(err => {
-      console.error(err);
-    });
+          dpPrice = data.Quotes[0].MinPrice;
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
 
-
+  }
 }
 
 const getRatesIn = () => {
@@ -82,17 +87,22 @@ const getRatesIn = () => {
   let departure = document.querySelector('#departure').value;
   let inbound = document.querySelector('#inbound').value;
 
-  return fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/${currency}/en-US/${destination}-sky/${origin}-sky/${departure}?inboundpartialdate=${inbound}`, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": `${kpi}`,
-      "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-    }
-  })
-    .then(res => res.json())
-    .then(data => {
-      let carrier = data.Carriers[0].Name;
-      displayTwo.innerHTML = `
+  if (currency === '' || origin === '' || destination === '' || departure === '' || inbound === '') {
+    return console.log('NULL');
+  } else {
+
+
+    return fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/${currency}/en-US/${destination}-sky/${origin}-sky/${departure}?inboundpartialdate=${inbound}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": `${kpi}`,
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        let carrier = data.Carriers[0].Name;
+        displayTwo.innerHTML = `
       <div class="flexer">
         <h2>Inbound Carrier:</h2>
         <h2>${carrier.toUpperCase()}</h2>
@@ -109,11 +119,12 @@ const getRatesIn = () => {
         <h4>Prices may vary from site-to-site</h4>
       </div>
       `
-    })
-    .catch(err => {
-      console.error(err);
-    });
+      })
+      .catch(err => {
+        console.error(err);
+      });
 
+  }
 
 }
 
